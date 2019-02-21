b8d8799136624b5aa91ae69784e914158f600f77
Gitanjali Mule <gitanjalidmule@gmail.com>
2019-02-21 02:21:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,18 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
+  i = 1
+  dwarves.each_with_index do |x|
+    puts "#{i} #{x}"
+    i += 1
+  end
+
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  array.collect do |word|
+    array << word[0].upcase + "!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
